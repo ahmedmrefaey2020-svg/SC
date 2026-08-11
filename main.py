6f633f463f0b0e73cdadd244b33be14309d5ec9e
ahmedmrefaey2020-svg <ahmedmrefaey2020@gmail.com>
2026-08-11 20:56:28 +0300

    Add pathlib import to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from contextlib import asynccontextmanager
-
+from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse, HTMLResponse
